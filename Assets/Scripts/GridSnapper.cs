afb61e9ccd6b21b5843ce891934b324b494caafb
Robert Pooley <robertcpooley@gmail.com>
2020-02-02 04:14:47 -0500

    Working on joint collider

--- Assets/Scripts/GridSnapper.cs
+++ Assets/Scripts/GridSnapper.cs
@@ -10,43 +10,53 @@ public class GridSnapper : MonoBehaviour {
 
     public int xs = 1, ys = 1, zs = 1;
 
+    public bool noScale;
+
     public bool runtimeCalculate;
 
-    public bool debugSnap;
+    private JointToggler jointToggler;
 
     // Start is called before the first frame update
     void Start() {
-        if (runtimeCalculate) {
-            Vector3 scale = new Vector3(xs * unitSize, ys * unitSize, zs * unitSize);
-            transform.localPosition = new Vector3(
-                x * unitSize + scale.x / 2,
-                y * unitSize + scale.y / 2,
-                z * unitSize + scale.z / 2
-            );
-        }
-        GridSnap();
-    }
+        jointToggler = GetComponent<JointToggler>();
 
-    // Update is called once per frame
-    void Update() {
-        if (debugSnap) {
-            debugSnap = false;
+        if (runtimeCalculate) {
+            GridSnap(x, y, z);
+        } else {
             GridSnap();
         }
     }
 
-    void GridSnap() {
+    // Update is called once per frame
+    void Update() { }
+
+    public void GridSnap() {
         Vector3 scale = new Vector3(xs * unitSize, ys * unitSize, zs * unitSize);
         Vector3 corner = transform.localPosition - scale / 2;
         Vector3 rawPos = corner / unitSize;
-        x = (int) Mathf.Round(rawPos.x);
-        y = (int) Mathf.Round(rawPos.y);
-        z = (int) Mathf.Round(rawPos.z);
+        int x = (int) Mathf.Round(rawPos.x);
+        int y = (int) Mathf.Round(rawPos.y);
+        int z = (int) Mathf.Round(rawPos.z);
+        GridSnap(x, y, z);
+    }
+
+    public void GridSnap(int x, int y, int z) {
+        Vector3 scale = new Vector3(xs * unitSize, ys * unitSize, zs * unitSize);
+        if (noScale) {
+            scale = Vector3.one;
+        }
+        this.x = x;
+        this.y = y;
+        this.z = z;
         transform.localPosition = new Vector3(
             x * unitSize + scale.x / 2,
             y * unitSize + scale.y / 2,
             z * unitSize + scale.z / 2
         );
         transform.localScale = scale;
+
+        if (jointToggler) {
+            jointToggler.OnGridSnap(this);
+        }
     }
 }
---
+++ Assets/Scripts/JointToggler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointToggler : MonoBehaviour {
+
+    public GameObject jointToggleCollider;
+
+    private GameObject jointCollider;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public void OnGridSnap(GridSnapper snapper) {
+        if (jointCollider) {
+            Destroy(jointCollider);
+        }
+
+        jointCollider = Instantiate(jointToggleCollider);
+        Transform t = jointCollider.transform;
+        t.parent = transform.parent; // set part's parent object as parent
+        t.localPosition = transform.localPosition;
+        t.localScale = transform.localScale * 1.5f;
+    }
+}
